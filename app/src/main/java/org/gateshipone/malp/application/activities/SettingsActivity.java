818f7be87c49ef6f3f3d8629e14b2bce3a17f98e
Matthias Weiss <matthias@more-onion.com>
2018-07-29 19:29:51 +0200

    Fixed crahs when opening the artwork settings view

--- app/src/main/java/org/gateshipone/malp/application/activities/SettingsActivity.java
+++ app/src/main/java/org/gateshipone/malp/application/activities/SettingsActivity.java
@@ -15,7 +15,9 @@ public class SettingsActivity extends AppCompatActivity implements SettingsFragm
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getSupportFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+        setContentView(R.layout.activity_settings);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.settings_container, new SettingsFragment()).commit();
     }
 
     @Override
